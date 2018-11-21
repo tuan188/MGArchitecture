34325624a407f36ac58ae6166479a16ec41b80c2
Tuan Truong <tuan188@gmail.com>
2018-11-21 09:45:27 +0700

    Fix PagingInfo init

--- MGArchitecture/Model/PagingInfo.swift
+++ MGArchitecture/Model/PagingInfo.swift
@@ -3,6 +3,11 @@ import OrderedSet
 public struct PagingInfo<T> {
     public let page: Int
     public let items: [T]
+    
+    public init(page: Int, items: [T]) {
+        self.page = page
+        self.items = items
+    }
 }
 
 extension PagingInfo where T: Hashable {
